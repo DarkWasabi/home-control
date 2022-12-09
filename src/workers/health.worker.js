c853fe7d28421e2bcba4eab38abbec1629c7b0e0
Alex Andreiev <darkwasabj@gmail.com>
2022-12-10 00:12:01 +0200

    refactor worker

--- src/workers/health.worker.js
+++ src/workers/health.worker.js
@@ -5,42 +5,45 @@ const config = require('../config/config');
 class HealthWorker extends Worker {
     #errors = 0;
 
-    constructor() {
-        const errorHandler = () => {
-            this.#errors += 1;
-            if (this.#errors < 3) {
-                return;
-            }
-            if (this.#errors === 3) {
-                const telegramClient = axios.create({
-                    baseURL: config.telegramBotUrl,
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Charset': 'utf-8',
-                    }
-                });
-
-                telegramClient.post('/sendMessage', {
-                    chat_id: config.telegramChatId,
-                    text: '📵 There is no power supply!',
-                }).catch(error => console.log(error));
-            }
+    #errorHandler = () => {
+        this.#errors += 1;
+        if (this.#errors < 3) {
+            return;
         }
+        if (this.#errors === 3) {
+            const telegramClient = axios.create({
+                baseURL: config.telegramBotUrl,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Charset': 'utf-8',
+                }
+            });
 
-        super(() => setInterval(() => {
-            axios.get('http://host-176-38-7-39.b026.la.net.ua:8080')
-                .then((res) => {
-                    if (res.status > 400) {
-                        errorHandler();
-                        return;
-                    }
-                    this.#errors = 0;
-                })
-                .catch((error) => {
-                    console.error(error);
-                    errorHandler();
-                });
-        }, 5000));
+            telegramClient.post('/sendMessage', {
+                chat_id: config.telegramChatId,
+                text: '📵 There is no power supply!',
+            }).catch(error => console.log(error));
+        }
+    }
+
+    #createWorker = () => setInterval(() => {
+        axios.get('http://host-176-38-7-39.b026.la.net.ua:8080')
+            .then((res) => {
+                if (res.status > 400) {
+                    this.#errorHandler();
+                    return;
+                }
+                this.#errors = 0;
+            })
+            .catch((error) => {
+                console.error(error);
+                this.#errorHandler();
+            });
+    }, 5000)
+
+
+    constructor() {
+        super(this.#createWorker);
     }
 }
 
